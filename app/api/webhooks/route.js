76f54ad607941e384266bc2ac877065cc0c236f3
Pedro Canterini <550411+pcanterini@users.noreply.github.com>
2024-08-26 11:16:27 -0700

    update after adding webhooks

--- app/api/webhooks/route.js
+++ app/api/webhooks/route.js
@@ -1,21 +1,26 @@
 // The Replicate webhook is a POST request where the request body is a prediction object.
 // Identical webhooks can be sent multiple times, so this handler must be idempotent.
 
-import { NextResponse } from 'next/server';
-import { validateWebhook } from 'replicate';
+import { NextResponse } from "next/server";
+import { validateWebhook } from "replicate";
 
 export async function POST(request) {
   console.log("Received webhook...");
-  
+
   const secret = process.env.REPLICATE_WEBHOOK_SIGNING_SECRET;
 
   if (!secret) {
-    console.log("Skipping webhook validation. To validate webhooks, set REPLICATE_WEBHOOK_SIGNING_SECRET")
+    console.log(
+      "Skipping webhook validation. To validate webhooks, set REPLICATE_WEBHOOK_SIGNING_SECRET"
+    );
     const body = await request.json();
     console.log(body);
-    return NextResponse.json({ detail: "Webhook received (but not validated)" }, { status: 200 });
+    return NextResponse.json(
+      { detail: "Webhook received (but not validated)" },
+      { status: 200 }
+    );
   }
-  
+
   const webhookIsValid = await validateWebhook(request.clone(), secret);
 
   if (!webhookIsValid) {
